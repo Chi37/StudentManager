e8fb822925f3ea486fca04ca5ded36b32ff819f6
Chi <bechi1994@gmail.com>
2018-10-30 11:36:26 -0500

    Added read and save functions. This allows for student names to be added in a text file and the file is created and saved if there isn't already an existing file. Still have to work on making this better.

--- functions.py
+++ functions.py
@@ -1,6 +1,11 @@
 students = []
 
 
+def get_student_name():
+	student_name = input("Enter student name: ")
+	student_id = input("Enter student id: ")
+
+
 def get_students_titlecase():
 	students_titlecase = []
 	for student in students:
@@ -19,21 +24,44 @@ def add_student(name, student_id=0):
 
 
 student_list = get_students_titlecase()
-#to add students is wrritten like add_student("Mark")
-
-def insert_student_info():
-	student_name = input("Enter student name: ")
-	student_id = input("Enter student id: ")
-	add_student(student_name, student_id)
-	print_students_titlecase()
+#to add students is written like add_student("Mark")
 
 
 def get_user_input():
 	user_input = input("Do you want to add a student?")
 	if user_input == "yes":
 		insert_student_info()
-	else:
-		print("thanks")
 
-get_user_input()
+
+def save_file(student):
+	try:
+		f = open("students.txt","a")
+		f.write(student + "\n")
+		f.close()
+	except:
+		print("Could not save file")
+
+
+def read_file():
+	try:
+		f = open("students.txt","r")
+		for student in f.readlines():
+			add_student(student)
+			print("reading...")
+		f.close()
+	except Exception:
+		print("could not read file")
+
+    
+read_file()
+print_students_titlecase()
+
+
+student_name = input("Enter student name: ")
+student_id = input("Enter student id: ")
+
+
+add_student(student_name)
+save_file(student_name)
+
 
